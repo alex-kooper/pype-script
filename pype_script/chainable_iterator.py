9905314381f65be7ec5bc0a2a8e737479285475e
alex-kooper <alex_kooper@Kooper-MacBook.local>
2013-10-22 07:35:40 -0400

    First implementaion of FunctionWrapper

--- pype_script/chainable_iterator.py
+++ pype_script/chainable_iterator.py
@@ -9,3 +9,21 @@ class ChainableIterator(object):
     def __iter__(self):
         return self
 
+
+class FunctionWrapper(ChainableIterator):
+    def __init__(self, input_iterator=None):
+        super(FunctionWrapper, self).__init__(input_iterator)  
+        self.__wrapped_iterator = None
+
+    def __setattr__(self, name, value):
+        super(FunctionWrapper, self).__setattr__(name, value)
+
+        if name == 'input_iterator' and value:
+            self._create_iterator()    
+
+    def _create_iterator(self):
+        print 'creating iterator: ', self.input_iterator
+
+    def next():
+        self.__wrapped_iterator.next()
+
