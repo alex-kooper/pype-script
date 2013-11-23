a86dc6fd9b541480f8d6714bc9cac6bbb6f93e88
Alex <alex_kooper@gmail.com>
2013-11-23 16:00:28 -0500

    Added comments improved dedup

--- pype_script/dedup.py
+++ pype_script/dedup.py
@@ -1,5 +1,11 @@
 
 class dedup(object):
+    """ Remove duplicated elements in the stream according to function key()
+
+    If more than one elements that are equal according to function key() are in
+    the stream, it leaves only the first one out of the series of equal 
+    elements. If key function is not specified, it will compare objects
+    """
     def __init__(self, key=None, input_iterable=None):
         if not key:
             key = lambda x: x
@@ -7,21 +13,22 @@ class dedup(object):
         self.key = key
         self.input_iterable = input_iterable
 
-        self.__previous_keys = None
-        self.__input_iterator = None
-
     def __iter__(self):
+        return DedupIterator(self.key, iter(self.input_iterable))
+
+class DedupIterator(object):
+    def __init__(self, key, input_iterator):
+        self.__key = key
+        self.__input_iterator = input_iterator
         self.__previous_keys = set()
-        self.__input_iterator = iter(self.input_iterable)
-        return self
 
     def next(self):
         el = self.__input_iterator.next()
-        key = self.key(el)
+        key = self.__key(el)
 
         while key in self.__previous_keys:
             el = self.__input_iterator.next()
-            key = self.key(el)
+            key = self.__key(el)
 
         self.__previous_keys.add(key)
         return el
