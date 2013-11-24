5406f6da4791c5622f7bd6dc6ddb5dfdfc2df993
Alex <alex_kooper@gmail.com>
2013-11-24 14:00:11 -0500

    Closable Iterators implementation

---
+++ pype_script/iterator.py
@@ -0,0 +1,16 @@
+
+class ClosableIterator(object):
+    def close(self):
+        try:
+            self.input_iterator.close()
+        except AttributeError:
+            pass
+
+class ClosableIteratorWrapper(ClosableIterator):
+    def __init__(self, wrapped_iterator, input_iterator=None):
+        self.wrapped_iterator = wrapped_iterator
+        self.input_iterator = input_iterator
+
+    def next(self):
+        return self.wrapped_iterator.next()
+
