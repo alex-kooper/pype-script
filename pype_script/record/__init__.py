8ee2528483895392294654f4975bbbda831168f7
Alex <alex_kooper@gmail.com>
2013-12-01 20:09:16 -0500

    Added column function

--- pype_script/record/__init__.py
+++ pype_script/record/__init__.py
@@ -2,5 +2,8 @@
 from pype_script.record.records import Record, SlotsRecord
 from pype_script.record.select_columns import select_columns, ColumnSelector
 from pype_script.record.dedup_by_columns import dedup_by_columns
-from pype_script.record.sort_by_columns import sort_by_columns, RecordComparator
+
+from pype_script.record.sort_by_columns import sort_by_columns
+from pype_script.record.sort_by_columns import column
+from pype_script.record.sort_by_columns import RecordComparator
 
--- pype_script/record/sort_by_columns.py
+++ pype_script/record/sort_by_columns.py
@@ -4,23 +4,26 @@ Sort records by specified columns. Each column described by a tuple:
 
 It is supposed to be used in a pipline like this:
 
-    pipeline_element | sort_by_columns('a', ('b', True), 'c') | pipeline_element
+    pipeline_element | sort_by_columns('a', ('b', True), column('c', key=len)) |
+    pipeline_element
 """
 
 from pype_script import sort
+
+def column(name, reverse=False, key=None):
+    """Create a column descriptor"""
+    if not key:
+        key = lambda x: x
+
+    return (name, reverse, key)
+
 
 class RecordComparator(object):
     """
     A callable object that compares records by specified columns
     """
     def __init__(self, *args):
-        def sort_descriptor(name, reverse=False, key=None):
-            if not key:
-                key = lambda x: x
-
-            return (name, reverse, key)
-
-        self.__args = [sort_descriptor(*d) for d in args]
+        self.__args = [column(*d) for d in args]
 
     def __call__(self, record1, record2):
         for name, reverse, key in self.__args:
